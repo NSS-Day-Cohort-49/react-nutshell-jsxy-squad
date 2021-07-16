74211742a485da35334c4176fbfed5932c71723f
Joel Beckum <joelbeckum@gmail.com>
2021-07-16 11:09:50 -0400

    Added PlannedEventProvider to populate plannedEvent modules with external data

---
+++ src/components/plannedEvents/PlannedEventsProvider.js
@@ -0,0 +1,39 @@
+import React, { useState, createContext } from "react"
+
+export const PlannedEventContext = createContext()
+
+export const PlannedEventProvider = () => {
+    const [plannedEvents, getPlannedEvents ] = useState([])
+
+    const getPlannedEvents = () => {
+        return fetch("localhost:8088/plannedEvents")
+        .then(response => response.json())
+        .then(setPlannedEvents)
+    }
+
+    const addPlannedEvent = eventObj => {
+        return fetch("localhost:8088/plannedEvents", {
+            method: "POST",
+            headers: {
+                "Content-Type:": "application.json"
+            },
+            body: JSON.Stringify(eventObj)
+        })
+        .then(getPlannedEvents)
+    }
+
+    const removePlannedEvent = id => {
+        return fetch(`localhost:8088/plannedEvents/${id}`, {
+            method: "DELETE"
+        })
+        .then(getPlannedEvents)
+    }
+
+    return (
+        <PlannedEventContext.Provider value={{
+            plannedEvents, getPlannedEvents, addPlannedEvent, removePlannedEvent
+        }}>
+            {props.children}
+        </PlannedEventContext.Provider>
+    )
+}
